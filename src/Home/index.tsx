589716c0abe9af38ba9676eb7fe710fb51e17d79
Caio Querino <caioquerino04@gmail.com>
2025-08-13 06:45:21 -0300

    adicionando componente de input

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,17 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { Image, View } from 'react-native';
 import { Button } from '@/components/Button/'; 
+import { Input } from '@/components/Input';
 import { styles } from './styles';
 
 export function Home() {
   return (
     <View style={styles.container}>
+
       <Image source={require('../assets/logo.png')} style={styles.logo} />      
       <StatusBar style="auto" />
 
-      <Button title="Adicionar" />
-      <Button title="Deletar" />
+      <View style={styles.form}>
+        <Input placeholder='O que você precisa comprar?'/>
+        <Button title="Entrar" />
+      </View>
 
+      <View style={styles.content}>
+
+      </View>
     </View>
   );
 }
--- src/Home/styles.ts
+++ src/Home/styles.ts
@@ -3,15 +3,28 @@ import { StyleSheet } from 'react-native'
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#d0d2d8',
     alignItems: 'center',
+    backgroundColor: '#d0d2d8',
     paddingTop: 62,
-    padding: 24,
-    gap: 24
   },
-
-    logo: {
+  logo: {
     height: 34,
     width: 134,
   },
-});
+  form: {
+    width: '100%',
+    paddingHorizontal: 16,
+    gap: 7,
+    marginTop: 42,
+  },
+  content: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: '#FFFFFF',
+    borderTopLeftRadius: 24,
+    borderTopRightRadius: 24,
+    padding: 24,
+    paddingTop: 32,
+    marginTop: 24,
+  }
+})
