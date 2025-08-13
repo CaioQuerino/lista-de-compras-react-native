944de42fa7634a9694cba25ca295fd1804123ac0
Caio Querino <caioquerino04@gmail.com>
2025-08-13 07:30:16 -0300

    atualizando componente Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,14 +1,14 @@
-import { TouchableOpacity, Text } from "react-native";
-import { styles } from "./styles";
+import { Text, TouchableOpacity, TouchableOpacityProps } from "react-native"
+import { styles } from "./styles"
 
-type Props = {
-    title: string
+type Props = TouchableOpacityProps & {
+  title: string
 }
 
-export function Button( { title }: Props) {
-    return(
-        <TouchableOpacity style={styles.container} activeOpacity={0.8}>
-            <Text style={styles.title}>{title}</Text>
-        </TouchableOpacity>
-    )
+export function Button({ title, ...rest }: Props) {
+  return (
+    <TouchableOpacity style={styles.container} activeOpacity={0.8} {...rest}>
+      <Text style={styles.title}>{title}</Text>
+    </TouchableOpacity>
+  )
 }
