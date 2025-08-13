9b4f7814b7c60075091b49672d36427694592a55
Caio Querino <caioquerino04@gmail.com>
2025-08-13 07:29:52 -0300

    adicionando componente de items

--- src/App/Home/styles.ts
+++ src/App/Home/styles.ts
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet } from "react-native"
 
 export const styles = StyleSheet.create({
   container: {
@@ -26,5 +26,36 @@ export const styles = StyleSheet.create({
     padding: 24,
     paddingTop: 32,
     marginTop: 24,
-  }
-})
+  },
+  header: {
+    width: '100%',
+    flexDirection: 'row',
+    gap: 12,
+    borderBottomWidth: 1,
+    borderBottomColor: '#E4E6EC',
+    paddingBottom: 12,
+  },
+  clearButton: {
+    marginLeft: 'auto',
+  },
+  clearText: {
+    fontSize: 12,
+    color: '#828282',
+    fontWeight: 600,
+  },
+  separator: {
+    width: "100%",
+    height: 1,
+    backgroundColor: "#EEF0F5",
+    marginVertical: 16,
+  },
+  listContent: {
+    paddingTop: 24,
+    paddingBottom: 62,
+  },
+  empty: {
+    fontSize: 14,
+    color: "#808080",
+    textAlign: 'center'
+  },
+});
